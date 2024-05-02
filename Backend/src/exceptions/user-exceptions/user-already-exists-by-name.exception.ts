11c458041e08e280d57eba71064c15ec79aebcb0
ocsoares <casoares016@gmail.com>
2024-05-02 16:47:26 -0300

    chore: delete useless user exception

--- Backend/src/exceptions/user-exceptions/user-already-exists-by-name.exception.ts
+++
@@ -1,7 +0,0 @@
-import { BadRequestException } from "@nestjs/common";
-
-export class UserAlreadyExistsByNameException extends BadRequestException {
-    constructor() {
-        super("Already exists a user registered with this name !");
-    }
-}

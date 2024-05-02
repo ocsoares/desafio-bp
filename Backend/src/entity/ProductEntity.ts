638cc9d525c6b7a9cb1622a67189888f297dd0a9
ocsoares <casoares016@gmail.com>
2024-05-02 19:10:15 -0300

    feat: remove description field and add cnpj field to Product model

--- Backend/src/entity/ProductEntity.ts
+++ Backend/src/entity/ProductEntity.ts
@@ -2,7 +2,7 @@ export class ProductEntity {
     constructor(
         public readonly name: string,
         public readonly brand: string,
-        public readonly description: string,
+        public readonly cnpj: string,
         public readonly price: number,
     ) {}
 }

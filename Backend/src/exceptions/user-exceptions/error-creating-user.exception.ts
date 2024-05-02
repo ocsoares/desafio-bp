8fb054dd37b11ff36b63fdb7440de61ab606a7ef
ocsoares <casoares016@gmail.com>
2024-05-02 16:46:36 -0300

    chore: delete useless user exception

--- Backend/src/exceptions/user-exceptions/error-creating-user.exception.ts
+++
@@ -1,7 +0,0 @@
-import { InternalServerErrorException } from "@nestjs/common";
-
-export class ErrorCreatingUserException extends InternalServerErrorException {
-    constructor() {
-        super("Error creating user !");
-    }
-}
